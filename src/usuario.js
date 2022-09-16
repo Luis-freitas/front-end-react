eebe53037eb55584b5f4e9501361ded03cc6790d
Luis Freitas <luisfreitaso2001@gmail.com>
2022-09-16 00:48:30 -0300

    Criando formulario para usuario

--- src/usuario.js
+++ src/usuario.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-export default function Usuario(){
+export default function Usuario() {
+
+  function cadastrarUsuario(e) {
+    e.preventDefault()
+    console.log('cadastrou o usuario')
+    alert('Enviou nome:, ${this.state.value}');
+  }
+
+
   return (
-      <div>
-          <h1>Usuário</h1>
-          <Link to="/">retornar a página inicial</Link>
-      </div>
+    <div>
+      <h1>Usuário</h1>
+      <form onSubmit={cadastrarUsuario}>
+
+        <label>
+          Nome:
+          <input type="text"/>
+        </label>
+        <input type="submit" value="Enviar formulário!" />
+
+      </form>
+
+
+
+      <h1></h1>
+      <Link to="/">retornar a página inicial</Link>
+    </div>
+
+
   );
 }
+
